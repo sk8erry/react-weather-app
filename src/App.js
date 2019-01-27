0348fb186f749ce7117490e3ec07b7b2d8b365fd
Ricky Yang <grandruiqi@hotmail.com>
2019-01-26 20:37:19 -0800

    work in progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Weather from './components/Weather'
 const apiKey = '41b267bdffde95a9a783cb9e23a9b0d7'
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    humidity: undefined,
+    description: undefined,
+    city: undefined,
+    country: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault()
     const city = e.target.elements.city.value
@@ -13,6 +22,14 @@ class App extends React.Component {
     const apiCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`)
     const data = await apiCall.json()
     console.log(data)
+    this.setState({
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      city: data.name,
+      country: data.sys.country,
+      error: ''
+    })
   }
 
   render() {
@@ -20,7 +37,14 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     )
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,9 +2,20 @@ import React from 'react'
 
 class Weather extends React.Component {
   render() {
-    return(
-      <div>Weather</div>
-    )
+    if (this.props.temperature) {
+      return(
+        <div>
+          <p>Temperature: {this.props.temperature}</p>
+          <p>Location: {this.props.city}, {this.props.country}</p>
+          <p>Humidity: {this.props.humidity}</p>
+          <p>Conditions: {this.props.description}</p>
+        </div>
+      )
+    } else {
+      return(
+        null
+      )
+    }
   }
 }
 
