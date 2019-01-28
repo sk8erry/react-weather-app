bff7f025760b675f71f4dcf295a6119515eee69c
Ricky Yang <grandruiqi@hotmail.com>
2019-01-27 19:11:53 -0800

    done styling

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component {
   render() {
     return(
       <div>
-        <h1>Weather Find</h1>
-        <p>Find out temperature, conditions, and more</p>
+        <h1 className="title-container__title">Weather Find</h1>
+        <h3 className="title-container__subtitle">Find out temperature, conditions, and more</h3>
       </div>
     )
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,11 +4,19 @@ class Weather extends React.Component {
   render() {
     if (this.props.temperature) {
       return(
-        <div>
-          <p>Temperature: {this.props.temperature}</p>
-          <p>Location: {this.props.city}, {this.props.country}</p>
-          <p>Humidity: {this.props.humidity}</p>
-          <p>Conditions: {this.props.description}</p>
+        <div className="weather__info">
+          <p className="weather__key">
+          Temperature: <span className="weather__value">{this.props.temperature}&deg;C</span>
+          </p>
+          <p className="weather__key">
+            Location: <span className="weather__value">{this.props.city}, {this.props.country}</span>
+          </p>
+          <p className="weather__key">
+            Humidity: <span className="weather__value">{this.props.humidity}</span>
+          </p>
+          <p className="weather__key">
+            Conditions: <span className="weather__value">{this.props.description}</span>
+          </p>
         </div>
       )
     } else {
